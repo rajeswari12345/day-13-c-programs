088c44d9199fadc1d21e74cf6db1184117313643
rajeswari12345 <103126024+rajeswari12345@users.noreply.github.com>
2022-05-09 17:17:53 +0530

    day13uc2

---
+++ day13 uc2.cs
@@ -0,0 +1,25 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+namespace Demo
+{
+    class MyApplication
+    {
+
+        public class FloatMax
+        {
+        static void Main(string[] args)
+        {
+              
+                float f1 = 134.45E-2f;
+                Console.WriteLine(f1);
+                float f2 = 137.54E-2f;
+                Console.WriteLine(f2);
+                float f3 = 124.59E-2f;
+                Console.WriteLine(f3);
+                Console.WriteLine("The Max float value is: " + f);
+            }
+        }
+
